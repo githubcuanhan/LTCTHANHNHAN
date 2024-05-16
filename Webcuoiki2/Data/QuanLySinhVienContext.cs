b1cef2be86e626dcc7227fa7a84f292073d45107
githubcuanhan <nhannguyen2003nguyennhan@gmail.com>
2024-05-16 21:58:51 +0700

    update contextdatabase

---
+++ Webcuoiki2/Data/QuanLySinhVienContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webcuoiki2.Models;
+using Webcuoiki2;
+namespace Webcuoiki2.Data
+{
+    public class QuanLySinhVienContext : DbContext
+    {
+        public QuanLySinhVienContext() : base("name=QuanLySinhVien") { }
+
+        public DbSet<SinhVien> SinhViens { get; set; }
+        public DbSet<LopSinhHoat> LopSinhHoats { get; set; }
+        public DbSet<LopHocPhan> LopHocPhans { get; set; }
+        public DbSet<GiaoVien> GiaoViens { get; set; }
+        public DbSet<Khoa> Khoa { get; set; }
+        public DbSet<Truong> Truong { get; set; }
+    }
+}
