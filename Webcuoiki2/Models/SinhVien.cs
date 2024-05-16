bea3bd42916ef22cfa73d6f7857a8f990cd6f729
githubcuanhan <nhannguyen2003nguyennhan@gmail.com>
2024-05-16 18:16:53 +0700

    Baitap

---
+++ Webcuoiki2/Models/SinhVien.cs
@@ -0,0 +1,15 @@
+﻿namespace Webcuoiki2.Models
+{
+    public class SinhVien
+    {
+        public int MaSV { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public bool GioiTinh { get; set; }
+        public string DiaChi { get; set; }
+        public string SDT { get; set; }
+        public string Email
+
+
+    }
+}
